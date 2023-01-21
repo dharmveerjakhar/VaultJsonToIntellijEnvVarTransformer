50cca6f6a8b0444d217448a1d3792caa8fcd16da
dharmveer-jakhar <dharmveer.jakhar@meesho.com>
2023-01-21 13:20:07 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string res;
+bool flag = false;
+
+void convertJsonConfigToIntellijEnvVar(string s) {
+
+	for(int i=0;i<s.size();i++) {
+		if(s[i]=='"') {
+			flag=1-flag;
+		} else if(s[i]==',') {
+			if(flag) {
+				res.push_back(s[i]);
+			} else {
+				res.push_back(';');
+			}
+		} else if(s[i]==':') {
+			if(flag) {
+				res.push_back(s[i]);
+			} else {
+				res.push_back('=');
+			}
+		} else if(s[i]==' ') {
+		    if(!(res.size() > 0 and (res[res.size()-1]=='=' or res[res.size()-1]==';'))) {
+		        res.push_back(s[i]);
+		    }
+		} else {
+		    res.push_back(s[i]);
+		}
+	}
+	
+}
+
+int main() {
+
+	string s;
+
+    while(getline(cin, s)) {
+        convertJsonConfigToIntellijEnvVar(s);
+    }
+    
+    cout<<res.substr(3, res.size()-4);
+
+	return 0;
